6dcff5f4a162360534012cc945aa4fd0c6e904b0
Arthur Servidor <86123199+ArthurServidor@users.noreply.github.com>
2025-09-25 00:45:47 -0300

    [DELTA-3] - start of unitofWork

---
+++ DeltaFour.Infrastructure/Repositories/AllRepositories.cs
@@ -0,0 +1,21 @@
+﻿using DeltaFour.Domain.IRepositories;
+using DeltaFour.Infrastructure.Context;
+
+namespace DeltaFour.Infrastructure.Repositories
+{
+    public class AllRepositories(AppDbContext context)
+    {
+        private IUserRepository? _userRepository;
+        private IUserAuthRepository? _userAuthRepository;
+
+        public IUserRepository UserRepository
+        {
+            get { return _userRepository ??= new UserRepository(context); }
+        }
+
+        public IUserAuthRepository UserAuthRepository
+        {
+            get { return _userAuthRepository ??= new UserAuthRepository(context); }
+        }
+    }
+}
