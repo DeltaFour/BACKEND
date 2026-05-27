6068236e2aea4499c67444b7793bb6b888b6a955
Gabriel Costal Fogo <gabrielcostal164@gmail.com>
2026-05-26 21:59:41 -0300

    fix: erro na migration

--- DeltaFour.Infrastructure/Migrations/20260513120000_add_system_company_and_roles.cs
+++ DeltaFour.Infrastructure/Migrations/20260513120000_add_system_company_and_roles.cs
@@ -4,68 +4,42 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace DeltaFour.Infrastructure.Migrations
 {
+    /// <inheritdoc />
     public partial class add_system_company_and_roles : Migration
     {
+        /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            var companyId = new Guid("11111111-1111-4111-8111-111111111111");
-            var createdBy = new Guid("22222222-2222-4222-8222-222222222222");
-
-            var roleRhId = new Guid("33333333-3333-4333-8333-333333333333");
-            var roleEmployeeId = new Guid("44444444-4444-4444-8444-444444444444");
-            var roleAdminId = new Guid("55555555-5555-4555-8555-555555555555");
-
             // Insert default system company
-            migrationBuilder.InsertData(
-                table: "company",
-                columns: new[] { "id", "name", "cnpj", "is_active", "created_by" },
-                values: new object[] { companyId, "System", "00000000000000", true, createdBy }
-            );
+            migrationBuilder.Sql(@"
+                INSERT INTO company (id, name, cnpj, is_active, created_at, created_by)
+                VALUES ('11111111-1111-4111-8111-111111111111', 'System', '00000000000000', 1, UTC_TIMESTAMP(), '22222222-2222-4222-8222-222222222222');
+            ");
 
             // Insert default roles linked to system company
-            migrationBuilder.InsertData(
-                table: "role",
-                columns: new[] { "Id", "name", "is_active", "company_id", "created_by" },
-                values: new object[,]
-                {
-                    { roleRhId, "RH", true, companyId, createdBy },
-                    { roleEmployeeId, "EMPLOYEE", true, companyId, createdBy },
-                    { roleAdminId, "ADMIN", true, companyId, createdBy }
-                }
-            );
+            migrationBuilder.Sql(@"
+                INSERT INTO role (Id, company_id, name, is_active, created_by, created_at)
+                VALUES 
+                    ('33333333-3333-4333-8333-333333333333', '11111111-1111-4111-8111-111111111111', 'RH', 1, '22222222-2222-4222-8222-222222222222', UTC_TIMESTAMP()),
+                    ('44444444-4444-4444-8444-444444444444', '11111111-1111-4111-8111-111111111111', 'EMPLOYEE', 1, '22222222-2222-4222-8222-222222222222', UTC_TIMESTAMP()),
+                    ('55555555-5555-4555-8555-555555555555', '11111111-1111-4111-8111-111111111111', 'ADMIN', 1, '22222222-2222-4222-8222-222222222222', UTC_TIMESTAMP());
+            ");
         }
 
+        /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            var roleRhId = new Guid("33333333-3333-4333-8333-333333333333");
-            var roleEmployeeId = new Guid("44444444-4444-4444-8444-444444444444");
-            var roleAdminId = new Guid("55555555-5555-4555-8555-555555555555");
+            migrationBuilder.Sql(@"
+                DELETE FROM role WHERE Id IN (
+                    '33333333-3333-4333-8333-333333333333',
+                    '44444444-4444-4444-8444-444444444444',
+                    '55555555-5555-4555-8555-555555555555'
+                );
+            ");
 
-            var companyId = new Guid("11111111-1111-4111-8111-111111111111");
-
-            migrationBuilder.DeleteData(
-                table: "role",
-                keyColumn: "Id",
-                keyValue: roleRhId
-            );
-
-            migrationBuilder.DeleteData(
-                table: "role",
-                keyColumn: "Id",
-                keyValue: roleEmployeeId
-            );
-
-            migrationBuilder.DeleteData(
-                table: "role",
-                keyColumn: "Id",
-                keyValue: roleAdminId
-            );
-
-            migrationBuilder.DeleteData(
-                table: "company",
-                keyColumn: "id",
-                keyValue: companyId
-            );
+            migrationBuilder.Sql(@"
+                DELETE FROM company WHERE id = '11111111-1111-4111-8111-111111111111';
+            ");
         }
     }
 }
